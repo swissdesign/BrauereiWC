c3f66f499658522e431002aa3e3b44b29280336d
swissdesign <Design@pascalheiniger.ch>
2025-07-30 14:05:48 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -44,7 +44,7 @@ $(function(){
 });
 
 /* start preloader */
-$(window).load(function(){
-	$('.preloader').fadeOut(1000); // set duration in brackets    
+$(window).on('load', function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
 });
 /* end preloader */
